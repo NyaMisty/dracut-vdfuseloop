b5f75c11554d54f620dbe6c81a3749f442d09b14
Misty <gyc990326@gmail.com>
2020-02-01 13:35:28 -0800

    Add license and fix typo

--- 90vdfuseloop/mount-loop.sh
+++ 90vdfuseloop/mount-loop.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Copyright © 2019-2020 NyaMisty
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 fstype="$(getarg rootfstype=)"
 if [ -z "$fstype" ]; then
     fstype="auto"
@@ -6,4 +22,5 @@ fi
 mountflag="$(getarg rootflags=)"
 if [[ ! -z "$mountflag" ]]; then
     mountflag="-o $mountflag"
+fi
 mount $mountflag -t $fstype /dev/rootfsloop /sysroot
